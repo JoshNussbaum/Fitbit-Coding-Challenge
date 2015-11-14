a25bc386e6a8b6abeda29ebbb49956d51b93eeac
Josh Nussbaum <joshn@joshn-mbp-pa.local>
2015-11-13 21:57:25 -0800

    Initial Commit

---
+++ CodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodingChallenge
+//
+//  Created by Josh Nussbaum on 11/13/15.
+//  Copyright (c) 2015 Josh Nussbaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
